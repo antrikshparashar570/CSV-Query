07c29d0141753c03206be022c3c9f8a5918249d7
antrikshparashar570 <42244094+antrikshparashar570@users.noreply.github.com>
2020-04-26 16:06:18 +0530

    Update pdf_to_csv.py

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -1,6 +1,7 @@
 import tabula
 
 class PDF_TO_CSV:
+
 	def converter(self, pdf_file_name):
 		# convert PDF into CSV file
 		tabula.convert_into("pdf_file_name", "output.csv", output_format="csv", pages='all')
